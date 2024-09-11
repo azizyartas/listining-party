cb3dcce3724391aa63e7c543ccb2a36084629e6d
azizyartas <67240879+azizyartas@users.noreply.github.com>
2024-09-12 01:32:24 +0300

    created: Some volt functions were created.

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,9 +1,24 @@
 <?php
 
+use App\Models\ListeningParty;
 use Livewire\Volt\Component;
 
 new class extends Component {
-    //
+    public string $name = '';
+    public $startTime;
+
+
+    public function createListeningParty()
+    {
+
+    }
+
+    public function with()
+    {
+        return [
+            'listening_parties' => ListeningParty::all(),
+        ];
+    }
 }; ?>
 
 <div>
