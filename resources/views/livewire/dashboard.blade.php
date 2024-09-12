c8509db261dc869056fb59aeb06f4baf9f85f9fa
azizyartas <67240879+azizyartas@users.noreply.github.com>
2024-09-12 20:46:37 +0300

    fixed: 'mediaURL' converted to 'media_url' and the code has been cleaned up.

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -21,7 +21,7 @@ new class extends Component {
         $this->validate();
 
         $episode = Episode::create([
-            'mediaURL' => $this->mediaURL,
+            'media_url' => $this->mediaURL,
         ]);
 
         $listeningParty = ListeningParty::create([
@@ -42,11 +42,25 @@ new class extends Component {
 <div class="flex items-center justify-center min-h-screen bg-slate-50">
     <div class="max-w-lg w-full px-4">
         <form wire:submit="createListeningParty" class="space-y-6">
-            <x-input wire:model="name" placeholder="Listening Party Name"/>
-            <x-input wire:model="mediaURL" placeholder="Podcast Episode URL"
-                     description="Direct Episode Link or Youtube Link, RSS Feeds will grab the latest episode"/>
-            <x-datetime-picker wire:model="startTime" placeholder="Listening Party Start Time"/>
-            <x-button emerald type="submit" label="Create Listening Party"/>
+            <x-input
+                wire:model="name"
+                placeholder="Listening Party Name"
+            />
+            <x-input
+                wire:model="mediaURL"
+                placeholder="Podcast RSS Feed URL"
+                description="Entering RSS Feed URL will grab the latest episode"
+            />
+            <x-datetime-picker
+                wire:model="startTime"
+                placeholder="Listening Party Start Time"
+                :min="now()->subDays(1)"
+            />
+            <x-button
+                emerald
+                type="submit"
+                label="Create Listening Party"
+            />
         </form>
     </div>
 </div>
