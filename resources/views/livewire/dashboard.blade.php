12e50b404ce821ad579f1f7267727c3a74c0a7e5
azizyartas <67240879+azizyartas@users.noreply.github.com>
2024-09-12 17:20:52 +0300

    created: Listening Party's form have been created and 'with' function determined return type to array.

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -13,7 +13,7 @@ new class extends Component {
 
     }
 
-    public function with()
+    public function with(): array
     {
         return [
             'listening_parties' => ListeningParty::all(),
@@ -21,6 +21,14 @@ new class extends Component {
     }
 }; ?>
 
-<div>
-    Hello World.
+<div class="flex items-center justify-center min-h-screen bg-slate-50">
+    <div class="max-w-lg w-full px-4">
+        <form wire:submit="createListeningParty" class="space-y-6">
+            <x-input wire:model="name" placeholder="Listening Party Name"/>
+            <x-input wire:model="mediaURL" placeholder="Podcast Episode URL"
+                     description="Direct Episode Link or Youtube Link, RSS Feeds will grab the latest episode"/>
+            <x-datetime-picker wire:model="startTime" placeholder="Listening Party Start Time"/>
+            <x-button emerald type="submit" label="Create Listening Party"/>
+        </form>
+    </div>
 </div>
