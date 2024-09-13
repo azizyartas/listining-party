db2665421880742cea9fe3f1ba2daf6f5c12ae34
azizyartas <67240879+azizyartas@users.noreply.github.com>
2024-09-14 01:32:13 +0300

    Add hasOneThrough relationship in ListeningParty model
    Introduced a new podcast relationship using the hasOneThrough method in the ListeningParty model. This allows linking ListeningParty to Podcast through the Episode model.

--- app/Models/ListeningParty.php
+++ app/Models/ListeningParty.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOneThrough;
 
 class ListeningParty extends Model
 {
@@ -24,5 +25,10 @@ class ListeningParty extends Model
     public function episode(): BelongsTo
     {
         return $this->belongsTo(Episode::class);
+    }
+
+    public function podcast(): HasOneThrough
+    {
+        return $this->hasOneThrough(Podcast::class, Episode::class, 'id', 'id', 'episode_id', 'podcast_id');
     }
 }
