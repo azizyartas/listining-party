1db8bc45ad677aab9bb4eefe3439df6a8ad74f2f
azizyartas <67240879+azizyartas@users.noreply.github.com>
2024-09-16 18:15:22 +0300

    Update layout title and add external resources
    Changed the page title to "Ear Feast - Let's Listen Together." Added preconnect link and stylesheet for fonts from Bunny Fonts and included WireUI scripts along with Vite assets.

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,7 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <title>{{ $title ?? 'Page Title' }}</title>
+        <title>Ear Feast - Let's Listen Together</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link
+            href="https://fonts.bunny.net/css?family=figtree:400,600|aleo:300,500,700|annie-use-your-telescope:400&display=swap"
+            rel="stylesheet" />
+
+        <wireui:scripts />
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         {{ $slot }}
